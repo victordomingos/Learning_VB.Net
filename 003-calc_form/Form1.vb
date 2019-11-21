ceacacecc5034881d2113d33ac58ca05cf6b6247
Victor Domingos <editor.arcosonline@gmail.com>
2019-11-21 11:09:59 +0000

    ...

--- 003-calc_form/Form1.vb
+++ 003-calc_form/Form1.vb
@@ -26,7 +26,7 @@
 
     Private Sub CalcularAvenca()
         Dim ano As Integer
-        Dim desc_idade As Integer
+        Dim desc_idade As Double
         Dim valor_base As Double
         Dim desc_escalao As Double
         Dim desc_socio As Double
@@ -37,43 +37,12 @@
         Integer.TryParse(txt_ano.Text, ano)
         Double.TryParse(txt_valor_base.Text, valor_base)
 
-
-        If chk_escalao.Checked Then
-            desc_escalao = 0.1
-        Else
-            desc_escalao = 0.0
-        End If
-
-        If chk_desc_socio.Checked Then
-            desc_socio = 0.05
-        Else
-            desc_socio = 0.0
-        End If
-
-        If chk_agravamento.Checked Then
-            valor_agrav = 12.0
-        Else
-            valor_agrav = 0.0
-        End If
-
-        If cmb_concelho.Text = "Amares" Then
-            desc_concelho = 0.2
-        Else
-            desc_concelho = 0.0
-        End If
-
-        If rad_distrito_sim.Checked Then
-            desc_distrito = 0.01
-        Else
-            desc_distrito = 0.0
-        End If
-
-        If ano >= 2000 Then
-            desc_idade = 0.1
-        Else
-            desc_idade = 0.0
-        End If
-
+        desc_escalao = If(chk_escalao.Checked, 0.1, 0.0)
+        desc_socio = If(chk_desc_socio.Checked, 0.05, 0.0)
+        valor_agrav = If(chk_agravamento.Checked, 12.0, 0.0)
+        desc_concelho = If(cmb_concelho.Text = "Amares", 0.2, 0.0)
+        desc_distrito = If(rad_distrito_sim.Checked, 0.01, 0.0)
+        desc_idade = If(ano >= 2000, 0.1, 0.0)
 
         Dim result As Double
         result = valor_base * (1 - desc_escalao) * (1 - desc_socio) * (1 - desc_concelho)
@@ -81,7 +50,7 @@
 
         result += valor_agrav
 
-        lbl_resultado.Text = result.ToString()
+        lbl_resultado.Text = result.ToString("C")
     End Sub
 
     Private Sub txt_valor_base_TextChanged(sender As Object, e As EventArgs) Handles txt_valor_base.TextChanged
