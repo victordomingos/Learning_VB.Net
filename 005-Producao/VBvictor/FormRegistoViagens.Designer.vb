accea3b1193a99e2d2953340f17191e104a58d59
Victor Domingos <editor.arcosonline@gmail.com>
2019-12-05 12:10:23 +0000

    Point 10

--- 005-Producao/VBvictor/FormRegistoViagens.Designer.vb
+++ 005-Producao/VBvictor/FormRegistoViagens.Designer.vb
@@ -24,18 +24,26 @@ Partial Class FormRegistoViagens
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormRegistoViagens))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
-        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.FicheiroToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AjudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
-        Me.btn_limpar_grid = New System.Windows.Forms.ToolStripButton()
-        Me.btn_listar = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.btn_limpar_bd = New System.Windows.Forms.ToolStripButton()
         Me.btn_lancar_registos = New System.Windows.Forms.ToolStripButton()
-        Me.ToolStripButton6 = New System.Windows.Forms.ToolStripButton()
-        Me.ToolStripComboBox1 = New System.Windows.Forms.ToolStripComboBox()
+        Me.btn_listar = New System.Windows.Forms.ToolStripButton()
+        Me.btn_limpar_grid = New System.Windows.Forms.ToolStripButton()
+        Me.btn_viagens2dias = New System.Windows.Forms.ToolStripButton()
+        Me.cmb_filtro = New System.Windows.Forms.ToolStripComboBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.grid1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txt_marca = New System.Windows.Forms.TextBox()
+        Me.txt_km = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
         Me.ToolStrip1.SuspendLayout()
+        CType(Me.grid1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -46,23 +54,6 @@ Partial Class FormRegistoViagens
         Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
-        '
-        'ToolStrip1
-        '
-        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton6, Me.btn_lancar_registos, Me.btn_listar, Me.btn_limpar_grid, Me.ToolStripButton2, Me.ToolStripComboBox1})
-        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
-        Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(800, 25)
-        Me.ToolStrip1.TabIndex = 1
-        Me.ToolStrip1.Text = "ToolStrip1"
-        '
-        'StatusStrip1
-        '
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 571)
-        Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
-        Me.StatusStrip1.TabIndex = 2
-        Me.StatusStrip1.Text = "StatusStrip1"
         '
         'FicheiroToolStripMenuItem
         '
@@ -76,29 +67,22 @@ Partial Class FormRegistoViagens
         Me.AjudaToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
         Me.AjudaToolStripMenuItem.Text = "Ajuda"
         '
-        'ToolStripButton2
+        'ToolStrip1
         '
-        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
-        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButton2.Name = "ToolStripButton2"
-        Me.ToolStripButton2.Size = New System.Drawing.Size(119, 22)
-        Me.ToolStripButton2.Text = "Nº Viagens 2 Dias"
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btn_limpar_bd, Me.btn_lancar_registos, Me.btn_listar, Me.btn_limpar_grid, Me.btn_viagens2dias, Me.cmb_filtro})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(800, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
         '
-        'btn_limpar_grid
+        'btn_limpar_bd
         '
-        Me.btn_limpar_grid.Image = CType(resources.GetObject("btn_limpar_grid.Image"), System.Drawing.Image)
-        Me.btn_limpar_grid.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.btn_limpar_grid.Name = "btn_limpar_grid"
-        Me.btn_limpar_grid.Size = New System.Drawing.Size(99, 22)
-        Me.btn_limpar_grid.Text = "Limpar Janela"
-        '
-        'btn_listar
-        '
-        Me.btn_listar.Image = CType(resources.GetObject("btn_listar.Image"), System.Drawing.Image)
-        Me.btn_listar.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.btn_listar.Name = "btn_listar"
-        Me.btn_listar.Size = New System.Drawing.Size(55, 22)
-        Me.btn_listar.Text = "Listar"
+        Me.btn_limpar_bd.Image = CType(resources.GetObject("btn_limpar_bd.Image"), System.Drawing.Image)
+        Me.btn_limpar_bd.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btn_limpar_bd.Name = "btn_limpar_bd"
+        Me.btn_limpar_bd.Size = New System.Drawing.Size(82, 22)
+        Me.btn_limpar_bd.Text = "Limpar BD"
         '
         'btn_lancar_registos
         '
@@ -108,25 +92,105 @@ Partial Class FormRegistoViagens
         Me.btn_lancar_registos.Size = New System.Drawing.Size(106, 22)
         Me.btn_lancar_registos.Text = "Lançar registos"
         '
-        'ToolStripButton6
+        'btn_listar
         '
-        Me.ToolStripButton6.Image = CType(resources.GetObject("ToolStripButton6.Image"), System.Drawing.Image)
-        Me.ToolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButton6.Name = "ToolStripButton6"
-        Me.ToolStripButton6.Size = New System.Drawing.Size(82, 22)
-        Me.ToolStripButton6.Text = "Limpar BD"
+        Me.btn_listar.Image = CType(resources.GetObject("btn_listar.Image"), System.Drawing.Image)
+        Me.btn_listar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btn_listar.Name = "btn_listar"
+        Me.btn_listar.Size = New System.Drawing.Size(55, 22)
+        Me.btn_listar.Text = "Listar"
         '
-        'ToolStripComboBox1
+        'btn_limpar_grid
         '
-        Me.ToolStripComboBox1.Items.AddRange(New Object() {"Mostrar todas as viagens", "Apenas viagens de 1 dia", "Apenas viagens de 2 ou mais dias"})
-        Me.ToolStripComboBox1.Name = "ToolStripComboBox1"
-        Me.ToolStripComboBox1.Size = New System.Drawing.Size(121, 25)
+        Me.btn_limpar_grid.Image = CType(resources.GetObject("btn_limpar_grid.Image"), System.Drawing.Image)
+        Me.btn_limpar_grid.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btn_limpar_grid.Name = "btn_limpar_grid"
+        Me.btn_limpar_grid.Size = New System.Drawing.Size(99, 22)
+        Me.btn_limpar_grid.Text = "Limpar Janela"
+        '
+        'btn_viagens2dias
+        '
+        Me.btn_viagens2dias.Image = CType(resources.GetObject("btn_viagens2dias.Image"), System.Drawing.Image)
+        Me.btn_viagens2dias.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btn_viagens2dias.Name = "btn_viagens2dias"
+        Me.btn_viagens2dias.Size = New System.Drawing.Size(119, 22)
+        Me.btn_viagens2dias.Text = "Nº Viagens 2 Dias"
+        '
+        'cmb_filtro
+        '
+        Me.cmb_filtro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmb_filtro.Items.AddRange(New Object() {"Mostrar todas as viagens", "Apenas viagens de 1 dia", "Apenas viagens de 2 ou mais dias"})
+        Me.cmb_filtro.Name = "cmb_filtro"
+        Me.cmb_filtro.Size = New System.Drawing.Size(200, 25)
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 571)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'grid1
+        '
+        Me.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grid1.Location = New System.Drawing.Point(0, 52)
+        Me.grid1.Name = "grid1"
+        Me.grid1.Size = New System.Drawing.Size(553, 516)
+        Me.grid1.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txt_km)
+        Me.GroupBox1.Controls.Add(Me.txt_marca)
+        Me.GroupBox1.Location = New System.Drawing.Point(571, 103)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 92)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Calcular distância"
+        '
+        'txt_marca
+        '
+        Me.txt_marca.Location = New System.Drawing.Point(94, 29)
+        Me.txt_marca.Name = "txt_marca"
+        Me.txt_marca.Size = New System.Drawing.Size(100, 20)
+        Me.txt_marca.TabIndex = 0
+        '
+        'txt_km
+        '
+        Me.txt_km.Location = New System.Drawing.Point(94, 56)
+        Me.txt_km.Name = "txt_km"
+        Me.txt_km.Size = New System.Drawing.Size(100, 20)
+        Me.txt_km.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Marca da viatura"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(9, 53)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calcular km"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'FormRegistoViagens
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 593)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.grid1)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.MenuStrip1)
@@ -137,6 +201,9 @@ Partial Class FormRegistoViagens
         Me.MenuStrip1.PerformLayout()
         Me.ToolStrip1.ResumeLayout(False)
         Me.ToolStrip1.PerformLayout()
+        CType(Me.grid1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -146,11 +213,17 @@ Partial Class FormRegistoViagens
     Friend WithEvents FicheiroToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AjudaToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ToolStrip1 As ToolStrip
-    Friend WithEvents ToolStripButton6 As ToolStripButton
+    Friend WithEvents btn_limpar_bd As ToolStripButton
     Friend WithEvents btn_lancar_registos As ToolStripButton
     Friend WithEvents btn_listar As ToolStripButton
     Friend WithEvents btn_limpar_grid As ToolStripButton
-    Friend WithEvents ToolStripButton2 As ToolStripButton
-    Friend WithEvents ToolStripComboBox1 As ToolStripComboBox
+    Friend WithEvents btn_viagens2dias As ToolStripButton
+    Friend WithEvents cmb_filtro As ToolStripComboBox
     Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents grid1 As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_km As TextBox
+    Friend WithEvents txt_marca As TextBox
 End Class
--- 005-Producao/VBvictor/MyDatabase.vb
+++ 005-Producao/VBvictor/MyDatabase.vb
@@ -13,67 +13,115 @@ Public Class MyDatabase
         End Using
     End Function
 
-    Public Function ObterTodosProdutos() As DataTable
-        Dim ssql = "SELECT p.id AS 'Cód.', 
-                           p.nome AS 'Designação', 
-                           c.nome AS 'Categoria', 
-                           p.qtd AS 'Qdt.' 
-                    FROM produto p 
-                        INNER JOIN categoria c 
-                            ON p.categoria_id=c.id"
-        Return ObterDados(ssql)
-    End Function
+    ''' <summary>
+    ''' Obtem uma lista das viagens a partir da base de dados, a partir do nº de dias
+    ''' 
+    ''' </summary>
+    ''' <param name="dias">Nº de dias que durou a viagem (usar 0 para incluir todas as viagens)</param>
+    ''' <param name="incluirViagensComMaisDias">Especificar se é pretendido nº exato de dias, 
+    '''                                         ou nº dias igual o superior 
+    ''' </param>
+    ''' <returns></returns>
+    Public Function ObterTodasViagens(dias As Integer, incluirViagensComMaisDias As Boolean) As DataTable
+        Dim filtro = " "
 
-    Public Function ObterProdutosDeCategoria(id_categoria As Integer) As DataTable
-        Dim ssql = $"SELECT p.id AS 'Cód.', 
-                           p.nome AS 'Designação', 
-                           c.nome AS 'Categoria', 
-                           p.qtd AS 'Qdt.' 
-                    FROM produto p 
-                        INNER JOIN categoria c 
-                            ON p.categoria_id=c.id
-                    WHERE c.id={id_categoria.ToString}"
-        Return ObterDados(ssql)
-    End Function
+        If dias <> 0 Then
+            filtro += " WHERE v.dias"
+            If incluirViagensComMaisDias Then
+                filtro += ">"
+            End If
 
+            filtro += $"={dias} "
+        End If
 
-    Public Function ObterTodasCategorias() As DataTable
-        Dim ssql = "SELECT id, nome 
-                    FROM categoria 
-                    ORDER BY nome"
-        Return ObterDados(ssql)
-    End Function
+        Dim ssql = $"SELECT v.id AS 'Cód.Viagem', 
+                           c.marca AS 'Veículo', 
+                           v.dias AS 'Duração', 
+                           v.km AS 'Distância' 
+                    FROM viagem v 
+                        INNER JOIN veiculo c 
+                            ON v.veiculo_id=c.id 
+                    {filtro} 
+                    ORDER BY v.id"
 
-    Public Function ObterCategoriasComProdutosEmStock() As DataTable
-        Dim ssql = "SELECT c.id as id, c.nome as nome
-                    FROM categoria c 
-                        INNER JOIN produto p 
-                            ON p.categoria_id=c.id 
-                    WHERE p.qtd > 0 
-                    ORDER BY nome"
         Return ObterDados(ssql)
     End Function
 
 
-    Public Function ObterProduto(id As Integer) As DataTable
-        Dim ssql = $"SELECT id, nome, categoria_id, qtd
-                    FROM produto
-                    WHERE id = {id}"
-        Return ObterDados(ssql)
-    End Function
+    'Public Function ObterProdutosDeCategoria(id_categoria As Integer) As DataTable
+    '    Dim ssql = $"SELECT p.id AS 'Cód.', 
+    '                       p.nome AS 'Designação', 
+    '                       c.nome AS 'Categoria', 
+    '                       p.qtd AS 'Qdt.' 
+    '                FROM produto p 
+    '                    INNER JOIN categoria c 
+    '                        ON p.categoria_id=c.id
+    '                WHERE c.id={id_categoria.ToString}"
+    '    Return ObterDados(ssql)
+    'End Function
 
-    Public Function AtualizarProduto(id As Integer, nome As String, qtd As Integer, categoria_id As Integer) As Integer
+
+    'Public Function ObterTodasCategorias() As DataTable
+    '    Dim ssql = "SELECT id, nome 
+    '                FROM categoria 
+    '                ORDER BY nome"
+    '    Return ObterDados(ssql)
+    'End Function
+
+    'Public Function ObterCategoriasComProdutosEmStock() As DataTable
+    '    Dim ssql = "SELECT c.id as id, c.nome as nome
+    '                FROM categoria c 
+    '                    INNER JOIN produto p 
+    '                        ON p.categoria_id=c.id 
+    '                WHERE p.qtd > 0 
+    '                ORDER BY nome"
+    '    Return ObterDados(ssql)
+    'End Function
+
+
+    'Public Function ObterProduto(id As Integer) As DataTable
+    '    Dim ssql = $"SELECT id, nome, categoria_id, qtd
+    '                FROM produto
+    '                WHERE id = {id}"
+    '    Return ObterDados(ssql)
+    'End Function
+
+    'Public Function AtualizarProduto(id As Integer, nome As String, qtd As Integer, categoria_id As Integer) As Integer
+    '    Using c As New SqlConnection(Config.SC)
+    '        c.Open()
+    '        Dim ssql = "UPDATE produto
+    '                    SET nome=@nome, qtd=@qtd, categoria_id=@categoria_id
+    '                    WHERE id=@id;"
+
+    '        Using comando As New SqlCommand(ssql, c)
+    '            comando.Parameters.AddWithValue("@nome", nome)
+    '            comando.Parameters.AddWithValue("@qtd", qtd)
+    '            comando.Parameters.AddWithValue("@categoria_id", categoria_id)
+    '            comando.Parameters.AddWithValue("@id", id)
+
+    '            Try
+    '                comando.ExecuteNonQuery()
+    '            Catch
+    '                Return 1
+    '            End Try
+
+    '            Return 0
+    '        End Using
+    '    End Using
+    'End Function
+
+
+
+    Public Function InserirViagem(veiculo_id As String, dias As Integer, km As Integer) As Integer
         Using c As New SqlConnection(Config.SC)
             c.Open()
-            Dim ssql = "UPDATE produto
-                        SET nome=@nome, qtd=@qtd, categoria_id=@categoria_id
-                        WHERE id=@id;"
+            Dim ssql = "INSERT INTO viagem (veiculo_id, dias, km) 
+                        VALUES (@veiculo_id, @dias, @km);"
 
             Using comando As New SqlCommand(ssql, c)
-                comando.Parameters.AddWithValue("@nome", nome)
-                comando.Parameters.AddWithValue("@qtd", qtd)
-                comando.Parameters.AddWithValue("@categoria_id", categoria_id)
-                comando.Parameters.AddWithValue("@id", id)
+                comando.Parameters.AddWithValue("@veiculo_id", veiculo_id)
+                comando.Parameters.AddWithValue("@dias", dias)
+                comando.Parameters.AddWithValue("@km", km)
 
                 Try
                     comando.ExecuteNonQuery()
@@ -86,17 +134,43 @@ Public Class MyDatabase
         End Using
     End Function
 
-    Public Function InserirProduto(nome As String, qtd As Integer, categoria_id As Integer) As Integer
+    Public Function InserirVeiculo(marca As String) As Integer
         Using c As New SqlConnection(Config.SC)
             c.Open()
-            Dim ssql = "INSERT INTO produto (nome, qtd, categoria_id) 
-                        VALUES (@nome, @qtd, @categoria_id);"
+            Dim ssql = "INSERT INTO veiculo (marca) 
+                        VALUES (@marca);"
 
             Using comando As New SqlCommand(ssql, c)
-                comando.Parameters.AddWithValue("@nome", nome)
-                comando.Parameters.AddWithValue("@qtd", qtd)
-                comando.Parameters.AddWithValue("@categoria_id", categoria_id)
+                comando.Parameters.AddWithValue("@marca", marca)
 
+                Try
+                    comando.ExecuteNonQuery()
+                    Return ObterPrimeiroVeiculoPorMarca(marca)
+                Catch
+                    Return 1
+                End Try
+
+                Return 0
+            End Using
+        End Using
+    End Function
+
+
+    Public Function ObterPrimeiroVeiculoPorMarca(marca As String) As Integer
+        Dim ssql = $"SELECT TOP 1 id 
+                    FROM veiculo 
+                    WHERE marca='{marca}'"
+        Dim dt = ObterDados(ssql)
+        Return Integer.Parse(dt.Rows(0)(0))
+    End Function
+
+
+    Public Function ApagarViagensEVeiculos()
+        Using c As New SqlConnection(Config.SC)
+            c.Open()
+            Dim ssql = "DELETE FROM viagem; DELETE FROM veiculo;"
+
+            Using comando As New SqlCommand(ssql, c)
                 Try
                     comando.ExecuteNonQuery()
                 Catch
@@ -108,22 +182,23 @@ Public Class MyDatabase
         End Using
     End Function
 
-    Public Function DeleteProduct(id)
-        Using c As New SqlConnection(Config.SC)
-            c.Open()
-            Dim ssql = "DELETE FROM produto WHERE id=@id;"
+    Public Function ObterDistanciaTotalPorVeiculo(veiculo_id As Integer) As Integer
+        Dim ssql = $"SELECT SUM(km) 
+                    FROM viagem 
+                    WHERE veiculo_id={veiculo_id}"
+        Dim dt = ObterDados(ssql)
+        Dim v = dt.Rows(0)(0).ToString
+        Dim n = Integer.Parse(v)
+        Return n
+    End Function
 
-            Using comando As New SqlCommand(ssql, c)
-                comando.Parameters.AddWithValue("@id", id)
-
-                Try
-                    comando.ExecuteNonQuery()
-                Catch
-                    Return 1
-                End Try
-
-                Return 0
-            End Using
-        End Using
+    Public Function ContarViagensPorVeiculoEDuracao(veiculo_id As Integer, dias As Integer) As Integer
+        Dim ssql = $"SELECT COUNT(id) 
+                    FROM viagem 
+                    WHERE veiculo_id={veiculo_id} AND dias={dias}"
+        Dim dt = ObterDados(ssql)
+        Dim v = dt.Rows(0)(0).ToString
+        Dim n = Integer.Parse(v)
+        Return n
     End Function
 End Class
