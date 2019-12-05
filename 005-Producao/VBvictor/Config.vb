015d24f58fbd8bfd4c29efb7136bf37e6e175fe3
Victor Domingos <editor.arcosonline@gmail.com>
2019-12-05 09:34:09 +0000

    005

---
+++ 005-Producao/VBvictor/Config.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class Config
+    'fill this with the db connection string
+    Public Shared ReadOnly SC = "Data Source=;
+                                Initial Catalog=;
+                                User ID=;
+                                Password=;
+                                Connect Timeout=30;
+                                Encrypt=False;
+                                TrustServerCertificate=False;
+                                ApplicationIntent=ReadWrite;
+                                MultiSubnetFailover=False"
+End Class
